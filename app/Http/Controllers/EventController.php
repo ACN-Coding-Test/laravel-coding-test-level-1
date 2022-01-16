b7c78e57bc09702d02a63c0e31390c6e72d13912
Jassim <jassim@ideablitztech.com>
2022-01-16 19:35:48 +0530

    UI Test Completed

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -121,6 +121,84 @@ class EventController extends BaseController
         }
         return $this->sendResponse($data_response, $message);        
     }
+
+    public function event_list_web()
+    {
+        $event = Event::orderBy('createdAt', 'desc')->paginate($this->pagination_limit);
+        return view('event.event_list', [
+            'events' => $event,
+            'search' => '0',
+        ]);
+    }
+    
+    public function event_show_web($id)
+    {        
+        $event = Event::find($id); 
+        return view('event.event_show', compact('event'));
+    }
+    
+    public function event_edit_web($id)
+    {
+        $event = Event::findOrFail($id);
+        return view('event.event_edit', compact('event'));       
+    }
+
+    public function event_name_exist(Request $request)
+    {
+        $data_all               =           $request->all();  
+        if(isset($data_all['event_id'])){
+            if(isset($data_all['event_name'])){
+                $exist_value                        =       DB::table('events')->where('events.name', $data_all['event_name'])->whereNotIn('events.id',[$data_all['event_id']])->get()->count();          
+            }elseif(isset($data_all['event_slug'])){
+                $exist_value                        =       DB::table('events')->where('events.slug', $data_all['event_slug'])->whereNotIn('events.id',[$data_all['event_id']])->get()->count();
+            }
+        }else{
+            if(isset($data_all['event_name'])){
+                $exist_value                        =       DB::table('events')->where('events.name', $data_all['event_name'])->get()->count();          
+            }
+        }
+        if($exist_value > '0'){
+            $count_status                       =       '0';
+        }else{
+            $count_status                       =       'true';
+        }
+        echo $count_status;
+    }
+    
+    public function event_create_web()
+    {       
+        return view('event.event_create');
+    }
+
+    public function delete_event_web(Request $request)
+    {
+        $data_all = $request->all();  
+        $event    = Event::find($data_all['event_id']);
+        if($event){
+            $delete = $event->delete();
+            if($delete==true){
+                $data_response['id'] = $event->id;
+                $message             = 'event made soft deleted successfully.'; 
+            }else{
+                $data_response['id'] = $event->id;
+                $message             = 'something went wrong.';     
+            }
+        }else{
+            $data_response['id'] = $id;
+            $message             = 'this id not exist.';   
+        }
+        return $this->sendResponse($data_response, $message);        
+    }
+
+    public function search_event_web(Request $request)
+    {
+        $data_all = $request->all();     
+        $event = Event::where('events.id', 'like', '%' . $data_all['event_search_val'] . '%')->orWhere('events.name', 'like', '%' . $data_all['event_search_val'] . '%')->orWhere('events.slug', 'like', '%' . $data_all['event_search_val'] . '%')->orderBy('createdAt', 'desc')->paginate($this->pagination_limit);
+        return view('event.event_list', [
+            'events' => $event,
+            'search' => '1',
+        ]);    
+    }
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',                     [EventController::class, 'event_list_web']);
+Route::get('/events',               [EventController::class, 'event_list_web'])->name('event_list_web');
+Route::get('/events/create',        [EventController::class, 'event_create_web'])->name('event_create_web');
+Route::get('/events/{id}',          [EventController::class, 'event_show_web'])->name('event_show_web');
+Route::post('/event_name_exist',    [EventController::class, 'event_name_exist'])->name('event_name_exist');
+Route::get('events/{id}/edit',      [EventController::class, 'event_edit_web'])->name('event_edit_web');
+Route::put('events/{id}/edit',      [EventController::class, 'update_event'])->name('update_event');
+Route::post('events/event_store',   [EventController::class, 'create_event'])->name('event_store');
+Route::post('events/delete',        [EventController::class, 'delete_event_web'])->name('delete_event_web');
+Route::post('events/search',        [EventController::class, 'search_event_web'])->name('search_event_web');
+
+Route::get('/cache',                [EventController::class, 'event_list_cache'])->name('event_list_cache');
+Route::get('/remote',               [EventController::class, 'event_list_remote'])->name('event_list_remote');
+
