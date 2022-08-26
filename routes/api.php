70c0473b186b27a273bc8c166b75d6262fecce1f
shyju wing20 <shyjuc@wing20.com>
2022-08-26 13:36:53 +0530

    first test committed

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PassportAuthController;
+use App\Http\Controllers\API\EventController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('signup', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+
+
+Route::middleware('auth:api')->group( function() {
+    Route::resource('events', EventController::class);
+});
