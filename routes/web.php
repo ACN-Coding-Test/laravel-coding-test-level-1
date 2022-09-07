61eb94eef309512b44d6facbcad62c3a52585c87
Luqman <62147517+luqman1412@users.noreply.github.com>
2022-09-08 00:23:43 +0800

    add middleware to route view

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,17 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\EventController::class, 'index'])->name('home');
-
 Route::get('/events', [App\Http\Controllers\EventController::class, 'index'])->name('event.index');
-Route::get('/events/create', [App\Http\Controllers\EventController::class, 'create'])->name('event.create');
-Route::post('/events/create', [App\Http\Controllers\EventController::class, 'store'])->name('event.store');
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/events/create', [App\Http\Controllers\EventController::class, 'create'])->name('event.create');
+    Route::post('/events/create', [App\Http\Controllers\EventController::class, 'store'])->name('event.store');    
+    Route::get('/events/{event}/edit', [App\Http\Controllers\EventController::class, 'edit'])->name('event.edit');
+    Route::put('/events/{event}/edit', [App\Http\Controllers\EventController::class, 'update'])->name('event.update');
+    Route::delete('/events/{event}', [App\Http\Controllers\EventController::class, 'destroy'])->name('event.destroy');
+    
+});
+
 Route::get('/events/{event}', [App\Http\Controllers\EventController::class, 'show'])->name('event.show');
-Route::get('/events/{event}/edit', [App\Http\Controllers\EventController::class, 'edit'])->name('event.edit');
-Route::put('/events/{event}/edit', [App\Http\Controllers\EventController::class, 'update'])->name('event.update');
-Route::delete('/events/{event}', [App\Http\Controllers\EventController::class, 'destroy'])->name('event.destroy');
 
