389ee8cf0a47511657b8cde6f9e1855adb02f869
shihab <shihab.creatella@gmail.com>
2022-09-22 23:53:19 +0600

    add uuid in events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Event extends Model
 {
@@ -12,5 +13,15 @@ class Event extends Model
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
 
+    protected $keyType = 'string';
+
     protected $fillable = ['name', 'slug'];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string)Str::orderedUuid();
+        });
+    }
 }
