6160e722cd6e27869a04c9ff93ecc4d6cd645bc2
shihab <shihab.creatella@gmail.com>
2022-09-22 23:39:44 +0600

    add events table, model, factory

---
+++ app/Models/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+
+    protected $fillable = ['name', 'slug'];
+}
