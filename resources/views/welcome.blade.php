4d861b92398cffaab6451d6dbd46ac8ac8e69ded
shihab <shihab.creatella@gmail.com>
2022-09-23 18:52:32 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,9 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                <h2 class=" fixed top-0 right-0 px-6 py-4 sm:block">
-                        <a href="/events" class="text-sm text-gray-700 dark:text-gray-500 underline">Events</a>
-                </h2>
+
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
