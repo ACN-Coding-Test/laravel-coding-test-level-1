dec3989e05174eb455055d54d35ffe3fc8fa57ef
shihab <shihab.creatella@gmail.com>
2022-09-23 18:09:17 +0600

    update

---
+++ resources/views/events/mail/newEventMail.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+    # Hello!,
+
+    This is an automated email to notify that you have created new event
+
+    Regards,<br>
+    {{ config('app.name') }}
+@endcomponent
