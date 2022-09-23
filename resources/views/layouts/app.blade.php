71db81ca014e72d799e178123e38392c068bc0cf
shihab <shihab.creatella@gmail.com>
2022-09-23 11:17:25 +0600

    add events link in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,9 +32,16 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto"></ul>
+                    <ul class="navbar-nav me-auto">
 
+                    </ul>
 
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/events">Events</a>
+                                </li>
+                    </ul>
                 </div>
             </div>
         </nav>
