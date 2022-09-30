516373a8455389b7eb4e05b4d199799172a505d4
Karla Librero <karla@Karlas-MacBook-Pro.local>
2022-10-01 01:02:46 +0800

    Create Event API

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,6 +32,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+                
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api/events.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
