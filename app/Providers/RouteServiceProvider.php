e1edcfaef8b833cffb3fb37917bc5a8e31942546
Karla Librero <karla@Karlas-MacBook-Pro.local>
2022-10-01 16:39:42 +0800

    Redirect to Locations after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/events';
+    public const HOME = '/locations';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/events') }}">
+                <a class="navbar-brand" href="{{ url('/locations') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -53,10 +53,10 @@
                             @endif
                         @else
                             <li class="nav-item">
-                              <a class="nav-link" href="{{ url('events') }}">Events</a>
+                              <a class="nav-link" href="{{ url('locations') }}">Locations</a>
                             </li>
                             <li class="nav-item">
-                              <a class="nav-link" href="{{ url('locations') }}">Locations</a>
+                              <a class="nav-link" href="{{ url('events') }}">Events</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
