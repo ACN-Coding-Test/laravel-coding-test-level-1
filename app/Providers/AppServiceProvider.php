5a97223534f0366a1c9d6c4e054e91a20da47846
mohammadkamil <mohammadkamil1991@gmail.com>
2022-10-01 20:00:51 +0800

    update appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
         Event::observe(EventObserver::class);
     }
 }
