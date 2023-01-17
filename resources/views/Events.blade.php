262227c3280a751e42d8801a905f56cdb4ce5a0f
Darwin <darpart19@gmail.com>
2023-01-17 18:25:15 +0530

    delete function working

--- resources/views/Events.blade.php
+++ resources/views/Events.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         @livewireStyles
     </head>
     <body>
-        <table>
+        @if( $event_list )
+        <table id="events-table">
             <tr>
                 <td>Id</td>
                 <td>Name</td>
@@ -21,8 +26,30 @@
                 <td>{{ $value }}</td>
             @endforeach
             <td><button>Update</button></td>
-            <td><button>Delete</button></td>
+            <td><button id="delete-btn">Delete</button></td>
             </tr>
         @endforeach
         </table>
+        @endif
+        <script>
+            $(document).ready(function() {
+
+                $("#events-table").on('click','#delete-btn',function() {
+                
+                    var currentRow=$(this).closest("tr");
+                    var col1=currentRow.find("td:eq(0)").text();
+
+                    let response = fetch('http://127.0.0.1:8000/api/v1/events/' + col1, {
+                        method: 'DELETE',
+                        //body: formData
+                        }).then(response => response.json());
+                    
+                    if( response ) {
+
+                        location.reload();
+                    }
+                });
+            });
+        </script>
     </body>
+    </html>
