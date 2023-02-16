fe2682ac9a19b5f0082c0efb696d6b7597a25e7e
tgzhafri <tgzhafri@gmail.com>
2023-02-16 21:11:11 +0800

    add model, migration and uuid trait

---
+++ app/Models/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UsesUuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use HasFactory, UsesUuidTrait, SoftDeletes;
+}
